48d4cf69fa5d1ab107e801f5519cd3a3c4cf6ad6
svnxteen <97171606+svnxteen@users.noreply.github.com>
2024-03-28 00:57:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-de21f303-b9af-44ed-a99a-10b8352c687c/)
 
 
 echo "$curl_output" >> Update.tsx
